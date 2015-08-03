0c811238682f1bdb0e860f27d5bf625133adcb00
Chris Lawrence <lordsutch@gmail.com>
2015-08-03 00:26:00 -0400

    Add -j and -l options.

--- signmatch.py
+++ signmatch.py
@@ -26,6 +26,7 @@ from __future__ import print_function
 from multiprocessing import Pool
 from functools import partial
 
+import multiprocessing
 import os
 import sys
 import glob
@@ -36,8 +37,8 @@ import skimage
 
 import argparse
 import itertools
-
-NCPUS=6
+import traceback
+import uuid
 
 # Now let's use the detector as you would in a normal application.  First we
 # will load it from disk.
@@ -51,30 +52,57 @@ from skimage.color import rgb2gray
 from skimage.io import imread
 from skimage import img_as_ubyte
     
-def process_file(f, verbose=False):
+def process_file(f, verbose=False, link=None):
+    if not os.path.exists(f):
+        # Skip if not readable
+        return []
     # Cheat a bit to improve upsampling speed here... grayscale is faster
-    # img = imread(f)
+    img = imread(f)
+    img = rgb2gray(img)
     with warnings.catch_warnings():
         warnings.simplefilter('ignore')
-        img = img_as_ubyte(rgb2gray(imread(f)))
+        img = img_as_ubyte(img)
     #print(dir(img))
     dets = detector(img, 1) # Upsampling improves detection IME
     if dets: # We found a sign (or more!)
         if verbose:
             print('Found', len(dets), 'sign(s) in', f, 
                   [str(x) for x in dets], file=sys.stderr)
+        if link:
+            if not os.path.exists(link) and not os.path.isdir(link):
+                os.makedirs(link, 0o700)
+            fnamebase = os.path.basename(f)
+            linkname = os.path.join(link, fnamebase)
+            # Add a random prefix to avoid clashes, if needed
+            while os.path.exists(linkname):
+                linkname = os.path.join(link, str(uuid.uuid4())+'-'+fnamebase)
+            try:
+                os.link(f, linkname)
+            except OSError as e:
+                try:
+                    os.symlink(f, linkname)
+                except OSError as e:
+                    print('Unable to link',f,'to',link, file=sys.stderr)
+                    trackback.print_exc()
         return dets
     if verbose:
         print('No signs in', f, file=sys.stderr)
     return []
 
 if __name__ == '__main__':
+    NCPUS = multiprocessing.cpu_count()/2
     parser = argparse.ArgumentParser(description='detect images matching pattern')
     parser.add_argument('-s', '--show-filter', dest='showfilter',
                         default=False, action='store_true',
                         help='show the filter that will be applied')
     parser.add_argument('-v', '--verbose', dest='verbose', default=False,
                         action='store_true', help='include extra output to stderr')
+    parser.add_argument('-l', '--link', dest='link', default=None,
+                        action='store', type=str, metavar='DIR',
+                        help='link found files to DIR')
+    parser.add_argument('-j', '--parallel', dest='ncpus', default=NCPUS,
+                        action='store', type=int, metavar='JOBS',
+                        help='how many parallel tasks to run')
     parser.add_argument('files', metavar='FILE', type=str, nargs="*",
                         help='files to scan')
     args = parser.parse_args()
@@ -90,8 +118,8 @@ if __name__ == '__main__':
     for bit in args.files:
         filenames.extend( glob.glob(bit) )
 
-    partial_process = partial(process_file, verbose=args.verbose)
+    partial_process = partial(process_file, verbose=args.verbose, link=args.link)
         
-    p = Pool(NCPUS) ## Number of parallel processes to run
+    p = Pool(args.ncpus) ## Number of parallel processes to run
     status = p.map(partial_process, filenames)
     print("\n".join(itertools.compress(filenames, status)))
